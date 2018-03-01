42f19f9fee5deb09faf886a3a91203af088e489f
Divyanshu Singh <dsinecos@gmail.com>
2018-03-01 11:45:12 +0530

    Upload files and save them to disk with their original names

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var app = express()
 var multer = require('multer')
-var upload = multer({ dest: 'uploads/'})
 var path = require('path');
 
 var PORT = process.env.PORT || 2500;
@@ -18,25 +17,24 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + "/public/form.html");
 });
 
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+var upload = multer({
+    storage: storage
+});
+
 var multipleUploads = upload.fields([{ name: 'iconFile', maxCount: 1}, { name: 'imageFile', maxCount: 1}]);
 
 app.post('/submitForm', multipleUploads, function(req, res) {
     console.log("Form submission received");
-    console.log(req.body.title);
-    console.log(req.body.message);
-    console.log(req.body.url);
     console.log((req.files['iconFile']) ? "Icon received" : "Icon not received");
-    console.log((req.files['imageFile']) ? "Image received" : "Image not received");
-
-    console.log("Extracting file information");
-    console.info("fieldname : " + req.files['iconFile'][0]['fieldname']);
-    console.info("originalname : " + req.files['iconFile'][0]['originalname']);
-    console.info("encoding : " + req.files['iconFile'][0]['encoding']);
-    console.info("mimetype : " + req.files['iconFile'][0]['mimetype']);
-    console.info("size : " + req.files['iconFile'][0]['size']);
-    console.info("destination : " + req.files['iconFile'][0]['destination']);
-    console.info("filename : " + req.files['iconFile'][0]['filename']);
-    console.info("path : " + req.files['iconFile'][0]['path']);
 
     res.send("Form submission received");
 
