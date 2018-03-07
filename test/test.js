6d6f385a0d2eff6115b0237fdab48acda116dd01
Divyanshu Singh <dsinecos@gmail.com>
2018-03-07 19:04:58 +0530

    Submitting forms with file uploads using chai-http for API testing

--- test/test.js
+++ test/test.js
@@ -23,6 +23,7 @@ describe("Test File Upload", function () {
         chai
             .request(server)
             .post('/submitForm')
+            .field("title", "customValue")
             .attach('iconFile', fs.readFileSync(__dirname + '/test.png'), 'avatar.png')
             .attach('imageFile', fs.readFileSync(__dirname + '/Apple.jpeg'), 'apple.jpeg')
             .send(formData)
@@ -35,34 +36,18 @@ describe("Test File Upload", function () {
 
     })
 
-    it("Can submit params", function (done) {
+    // it("Can submit params", function (done) {
 
-        var id = 1234;
+    //     var id = 1234;
 
-        chai
-            .request(server)
-            .get('/testParam/' + id + '/test')
-            .end(function (err, res) {
-                expect(res.status).to.equal(200);
-                done();
-            });
+    //     chai
+    //         .request(server)
+    //         .get('/testParam/' + id + '/test')
+    //         .end(function (err, res) {
+    //             expect(res.status).to.equal(200);
+    //             done();
+    //         });
 
 
-    })
+    // })
 })
-
-// Loading a file into a variable
-
-// describe.skip("Test POST Notification request", function () {
-//     it("Form submitted without file upload", function (done) {
-//         var formData = {
-//             title: "Title",
-//             message: "Message",
-//             onClickUrl: "Redirect URL when notification is clicked",
-//         }
-
-//         chai
-//             .request(server)
-//             .post('/notifications')
-//     })
-// })
