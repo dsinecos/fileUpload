b8df34515369bd093148709ecc1fb06190ae9951
Divyanshu Singh <dsinecos@gmail.com>
2018-03-23 17:56:17 +0530

    Testing if I can create nested objects by using period separated form input variable names - No

--- server.js
+++ server.js
@@ -47,10 +47,14 @@ var upload = multer({
 var multipleUploads = upload.fields([{ name: 'iconFile', maxCount: 1 }, { name: 'imageFile', maxCount: 1 }]);
 
 app.post('/submitForm', multipleUploads, function (req, res) {
-    console.log(req.headers);
-    console.log(req.body);
-    console.log("Form submission received");
-    console.log((req.files['iconFile']) ? "Icon received" : "Icon not received");
+    // console.log(req.headers);
+    // console.log(req.body);
+    // console.log("Form submission received");
+    // console.log((req.files['iconFile']) ? "Icon received" : "Icon not received");
+
+    var testObj = req.body;
+
+    console.log(testObj);
 
     res.send("Form submission received");
 
